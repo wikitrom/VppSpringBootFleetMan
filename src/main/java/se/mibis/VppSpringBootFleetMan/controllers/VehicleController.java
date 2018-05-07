b6394012b55a850e2834c863b1c299e8d2d1d252
Mats Bergstroem <hackingtrom@gmail.com>
2018-05-07 11:15:29 +0200

    Change annotation from generic @RequestMapping(POST) to @PostMapping

--- src/main/java/se/mibis/VppSpringBootFleetMan/controllers/VehicleController.java
+++ src/main/java/se/mibis/VppSpringBootFleetMan/controllers/VehicleController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,7 +20,8 @@ public class VehicleController {
 	@Autowired
 	private VehicleRepository data;
 
-	@RequestMapping(value = "/newVehicle.html", method = RequestMethod.POST)
+	// @RequestMapping(value = "/newVehicle.html", method = RequestMethod.POST)
+	@PostMapping(value = "/newVehicle.html")
 	public String newVehicle(Vehicle vehicle) {
 		data.save(vehicle);
 		System.out.println("UABTROM: BLABLALBA");
